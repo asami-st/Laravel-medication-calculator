0de459b5510271f3e00edaaf02fccdf6c1843f0a
asami-st <ysas.27y@gmail.com>
2023-11-25 17:04:12 +0900

    update

--- database/migrations/2023_11_20_083543_create_patients_table.php
+++ database/migrations/2023_11_20_083543_create_patients_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->unsignedBigInteger('user_id');
             $table->timestamps();
 
-            // $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
