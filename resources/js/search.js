9b53588a7dde15fbd496b513788fb2fe7c57d322
asami-st <ysas.27y@gmail.com>
2023-11-27 22:59:03 +0900

    modify

--- resources/js/search.js
+++ resources/js/search.js
@@ -23,10 +23,8 @@ $(function() {
             });
         },
         minLength: 3,
-        select: function(event, ui) {
-            // 選択された項目のIDを隠しフィールドに設定
+        select: function(ui) {
             $medicationId.val(ui.item.value);
-            // サジェストのテキストを検索ボックスに設定
             $medicationSearch.val(ui.item.label);
 
             return false;
@@ -35,27 +33,27 @@ $(function() {
 
     $('#medication-search').on('keydown', function(e) {
         var items = $('#search-results .list-group-item');
-            if (e.keycode === 40) { // ↓
-                e.preventDefault();
-                selectedMedication = (selectedMedication + 1) % items.length;
+        if (e.keycode === 40) { // ↓
+            e.preventDefault();
+            selectedMedication = (selectedMedication + 1) % items.length;
+            items.removeClass('active');
+            items.eq(selectedMedication).addClass('active');
+        } else if (e.keycode === 38) { // ↑
+            e.preventDefault();
+            if (selectedMedication <= 0) {
+                selectedMedication = -1;
+                items.removeClass('active');// back to input field
+            } else {
+                selectedMedication--;
                 items.removeClass('active');
                 items.eq(selectedMedication).addClass('active');
-            } else if (e.keycode === 38) { // ↑
-                e.preventDefault();
-                if (selectedMedication <= 0) {
-                    selectedMedication = -1;
-                    items.removeClass('active');// back to inpu
-                } else {
-                    selectedMedication--;
-                    items.removeClass('active');
-                    items.eq(selectedMedication).addClass('active');
-                }
-            } else if (e.keycode === 13 && selectedMedication !== -1) { // Enter, show suggest
-                e.preventDefault();
-                $('#medication-search').val(items.eq(selectedMedication).text());
-                $searchResults.hide();
-                selectedMedication = -1; // reset
             }
+        } else if (e.keycode === 13 && selectedMedication !== -1) { // Enter, show suggest
+            e.preventDefault();
+            $('#medication-search').val(items.eq(selectedMedication).text());
+            $searchResults.hide();
+            selectedMedication = -1; // reset
+        }
     });
 
     $(document).on('click', function(e) {
